ce666891bd02357991c076ef3f33c489e8456800
JFAquinoPando <pandolcik@gmail.com>
2025-10-23 20:53:35 -0300

    Cambios finales aplicados, ya me gusta el modo oscuro

--- js/landing.js
+++ js/landing.js
@@ -1,5 +1,4 @@
-        const btnDark = document.getElementById('toggle-dark');
-        btnDark.addEventListener('click', function() {
-            document.body.classList.toggle('dark-mode');
-            btnDark.textContent = document.body.classList.contains('dark-mode') ? 'Modo Claro' : 'Modo Oscuro';
-        });
+document.getElementById('toggle-dark').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    this.textContent = document.body.classList.contains('dark-mode') ? 'Modo Claro' : 'Modo Oscuro';
+});
