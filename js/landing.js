c334d4a29994290795a406f14f83b0891c9400cc
JFAquinoPando <pandolcik@gmail.com>
2025-10-23 21:22:08 -0300

    feat: Agrega imagen de placeholder y mejora estilos en modo oscuro

--- js/landing.js
+++ js/landing.js
@@ -1,4 +1,15 @@
+const urlImagen = "https://i.ytimg.com/vi/G6Wj_okoujU/hqdefault.jpg"
+const resultado = await fetch(urlImagen)
+
+
 document.getElementById('toggle-dark').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
     this.textContent = document.body.classList.contains('dark-mode') ? 'Modo Claro' : 'Modo Oscuro';
 });
+
+if (resultado.status === 200 || resultado.status === 304) {
+    document.querySelector(".about-image")
+    .insertAdjacentHTML("afterbegin", 
+        `<img class="img-full" src='${urlImagen}'>`);    
+}
+
